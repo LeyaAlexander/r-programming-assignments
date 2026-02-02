729306bf6bd3dce644b86d3d9936b0d2549af220
LeyaAlexander <255267750+LeyaAlexander@users.noreply.github.com>
2026-02-02 02:12:55 -0500

    Module 3 Vectors and Data Frame

---
+++ Module3DataFrames.R
@@ -0,0 +1,8 @@
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
+Name
+ABC_poll_results <- c(4, 62, 51, 21, 2, 14, 15)
+ABC_poll_results
+CBS_poll_results <- c(12, 75, 43, 19, 1, 21, 19)
+CBS_poll_results
+polls <- data.frame(Name, ABC_poll_results, CBS_poll_results)
+polls
